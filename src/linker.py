03b5c9babff49ebc10393bbae54e79eb994bb439
spoonLead <xuxlaevoleg@yandex.ru>
2018-09-03 20:10:28 +0400

    Deleted the old version of the linker and player.mipmap

--- src/linker.py
+++ src/linker.py
@@ -1,5 +1,6 @@
 import os
 
+outFile = open('main.js', 'w', encoding = 'UTF-8')
 
 dirs = {
     "engine" : './engine',
@@ -7,18 +8,17 @@ dirs = {
     "game" : './game'
 }
 
-outFile = open('main.js', 'w', encoding = 'UTF-8')
-outCodeString = ''
+def toLink(dir, file):
+    outCodeString = ''
+    if file != 'linker.py' and file!= 'main.js' and file != 'linker2.py':
+        openFile = open(str(dir)+'/'+str(file), mode = 'r',encoding = 'UTF-8')
+        outCodeString += openFile.read()+'\n'
+        openFile.close()
+    outFile.write(outCodeString)
 
 for dir in dirs.values():
-    filesList = os.listdir(dir)
-    for file in filesList:
-        if file != 'linker.py' and file!= 'main.js':
-            openFile = open(str(dir)+'/'+str(file), mode = 'r',encoding = 'UTF-8')
-            outCodeString += openFile.read()+'\n'
-            openFile.close()
-
-
-
-outFile.write(outCodeString)
-outFile.close()
+    fileTree = os.walk(dir)
+    for file in fileTree:
+        for srcFiles in file[2]:
+            toLink(file[0], srcFiles)
+            print(str(file[0])+'/'+ str(srcFiles))
--- src/main.js
+++ src/main.js
@@ -1,3 +1,165 @@
+//--------------ENGINE--(RENDER--KEYLISTENER--MAPSCROLLING--INIT)----------//
+
+
+function render(){
+  for(var i = 0; i < objects.length; i++){
+    objects[i].draw();
+  }
+  player.draw();
+}
+
+function keyListener(obj){
+  window.onmousemove = function(e){
+    this.x = e.offsetX==undefined?e.layerX:e.offsetX-obj.width/2;
+    this.y = e.offsetY==undefined?e.layerY:e.offsetY-obj.height/2;
+
+    this.BCLen = Math.abs(this.x-obj.x);
+    this.ACLen = Math.abs(this.y-obj.y);
+    if(this.x>obj.x & this.y<obj.y){obj.beta = (Math.atan(this.BCLen/this.ACLen)*180)/Math.PI;}    //первая четверь
+    else if(this.x<obj.x & this.y<obj.y){obj.beta = 90 - Math.atan(this.BCLen/this.ACLen)*180/Math.PI + 270 ;}   //вторая четверть
+    else if(this.x<obj.x & this.y>obj.y){obj.beta = Math.atan(this.BCLen/this.ACLen)*180/Math.PI + 180;}   //третья четверть
+    else if(this.x>obj.x & this.y>obj.y){obj.beta = 90 - Math.atan(this.BCLen/this.ACLen)*180/Math.PI + 90;}   //четвёртая четверть
+    console.log(this.x, this.y, this.BCLen, this.ACLen, obj.beta);
+  }
+  window.onkeydown = function(e){
+    switch(e.keyCode){
+      case 37, 65:
+        obj.side.left = true;
+        obj.side.right = false;
+      break;
+      case 38, 87:
+        obj.side.up = true;
+        obj.side.down = false;
+      break;
+      case 39, 68:
+        obj.side.right = true;
+        obj.side.left = false;
+      break;
+      case 40, 83:
+        obj.side.down = true;
+        obj.side.up = false;
+      break;
+      case 16:  //shift - speedboost
+        obj.speed += obj.speedBoost;
+      break;
+      case 32:
+          jump = true;
+          obj.side.down = obj.side.up = obj.side.right = obj.side.left = true
+      break;
+    }
+  }
+  window.onkeyup = function(e){
+    switch(e.keyCode){
+      case 37, 65:
+        obj.side.left = false;
+      break;
+      case 38, 87:
+        obj.side.up = false;
+      break;
+      case 39, 68:
+        obj.side.right = false;
+      break;
+      case 40, 83:
+        obj.side.down = false;
+      break;
+      case 16:
+        obj.speed-= obj.speedBoost;
+      break;
+      case 32:
+        jump = false;
+      break;
+    }
+  }
+}
+
+function mapScrolling(obj){
+  if(obj.side.up == true ){
+    for(var i = 0; i < objects.length; i++){
+        objects[i].y += obj.speed;
+    }
+  }
+  if(obj.side.down == true ){
+    for(var i = 0; i < objects.length; i++){
+        objects[i].y -= obj.speed;
+    }
+  }
+  if(obj.side.left == true ){
+    for(var i = 0; i < objects.length; i++){
+        objects[i].x += obj.speed;
+    }
+  }
+  if(obj.side.right == true){
+    for(var i = 0; i < objects.length; i++){
+        objects[i].x -= obj.speed;
+    }
+  }
+}
+//^^^^^^^^^^^^^^ENGINE--(RENDER--KEYLISTENER--MAPSCROLLING!--INIT)^^^^^^^^^^//
+
+window.onload = init;
+
+//-----------------------DECLARATIONS---------------------//
+var canvas;
+var screen;
+
+var player; var playerSp = new Image(); playerSp.src = "./img/player.jpg";
+var enemy; var enemySp = new Image(); enemySp.src = "./img/enemy.jpg";
+var background; var backgroundSp = new Image(); backgroundSp.src = "./img/bg.jpg";
+
+var objects = [];
+
+//^^^^^^^^^^^^^^^^^^^^^^^DECLARATIONS^^^^^^^^^^^^^^^^^^^^^//
+
+//----------------------INITIALISATION--------------------//
+function init(){
+  canvas = document.getElementById("canvas"); //конвенция
+  screen = canvas.getContext("2d");
+  player = new Player(canvas.width/2-25, canvas.height/2-25);
+  objects.push(background = new background(), enemy = new Enemy());
+
+  game();       //игровой цикл
+}
+//^^^^^^^^^^^^^^^^^^^^^^INITIALISATION^^^^^^^^^^^^^^^^^^^^//
+
+
+
+
+//-----GAMELOOP-----//
+function game(){
+  screen.clearRect(0, 0, canvas.width, canvas.height);
+  document.getElementById("canvas").onmousehover = keyListener;
+
+  render();
+  keyListener(player);
+  mapScrolling(player);
+
+  requestAnimationFrame(game);  //ограничивает fps
+}
+
+
+
+function Enemy(){
+  this.x = 500;
+  this.y = 300;
+  this.width = 50;
+  this.height = 50;
+}
+
+Enemy.prototype.draw = function(){
+  screen.drawImage(enemySp, 0, 0, 50, 50, this.x, this.y, this.width, this.height);
+}
+
+
+function background() {
+  this.x = 0;
+  this.y = 0;
+}
+
+background.prototype.draw = function() {
+  screen.drawImage(backgroundSp, 0, 0, 1024, 1024, this.x, this.y, 1024, 1024);
+}
+
+
 //------CLASS PLAYER-------//
 class Player{
   constructor(x, y){
