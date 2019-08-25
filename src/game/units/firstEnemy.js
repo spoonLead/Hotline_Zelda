2c5e18a4a2984c8a1bd854755c5a737a7fb7e27f
spoonLead <xuxlaevoleg@yandex.ru>
2019-08-25 08:22:44 +0400

    Refactoring firstEnemy to class realisation

--- src/game/units/firstEnemy.js
+++ src/game/units/firstEnemy.js
@@ -1,10 +1,14 @@
-function Enemy(){
-  this.x = 500;
-  this.y = 300;
-  this.width = 50;
-  this.height = 50;
+class Enemy{
+  constructor(){
+    this.x = 500;
+    this.y = 300;
+    this.width = 50;
+    this.height = 50;
+  }
+  
+  draw(){
+    screen.drawImage(enemySp, 0, 0, 50, 50, this.x, this.y, this.width, this.height);
+  }
+
 }
 
-Enemy.prototype.draw = function(){
-  screen.drawImage(enemySp, 0, 0, 50, 50, this.x, this.y, this.width, this.height);
-}
