fd1d13c74b200c8b2825a58fd5a3cdee2e4c6b1d
spoonLead <xuxlaevoleg@yandex.ru>
2019-08-31 22:03:16 +0400

    Added global var CURRENTFRAME and counter for it

--- src/engine/core.js
+++ src/engine/core.js
@@ -4,6 +4,7 @@ var screen;
 
 const FPS = 30;
 const FRAMETIME = getFrameTimeForFPS(FPS);
+var CURRENTFRAME = 1;
 
 function getFrameTimeForFPS(fps){
   return (1000/fps);
@@ -17,10 +18,19 @@ function clearCanvas(){
 async function gameLoopStart(){
   while(true){
     game();
+    currentFrameCounter();
     await sleep(FRAMETIME);
   }
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function currentFrameCounter(){
+  if(CURRENTFRAME < FPS)
+    CURRENTFRAME += 1;
+  else
+    CURRENTFRAME = 1;
+
 }
